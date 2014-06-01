ddab90b1c47d1c4b45cbb014e25eca03cbb0d5eb
Mhd Sulhan <m.shulhan@gmail.com>
2014-06-01 07:45:09 +0700

    Add social icons (facebook, github, google-plus, linkedin, twitter).

--- config.php
+++ config.php
@@ -1,3 +1,11 @@
 <?php
 $wui["contents_dir"]	= "contents";
 $wui["homepage"]		= "/home";
+/* Order matter */
+$wui["social_icon"]		= [
+	"facebook"		=> ""
+,	"github"		=> "http://github.com/shuLhan"
+,	"google_plus"	=> "http://plus.google.com/+MhdSulhan"
+,	"linked"		=> "http://id.linkedin.com/in/muhamadsulhan/"
+,	"twitter"		=> "http://twitter.com/MhdSulhan"
+];
