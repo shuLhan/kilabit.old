519c4c4984e644e384b2fea6ddb20c3e2b1638cb
Mhd Sulhan <m.shulhan@gmail.com>
2014-07-25 10:39:47 +0700

    [wui] manage.content: auto create journal path by post date.

--- manage/content.php
+++ manage/content.php
@@ -8,13 +8,74 @@ require_once ("../wui.php");
 
 define ("APP_PATH", realpath (dirname (__FILE__) ."/../"));
 
+function create_node ($path, $title)
+{
+	if (file_exists ($path)) {
+		return;
+	}
+
+	mkdir ($path);
+
+	$contents = <<<EOF
+<html>
+<head>
+<meta name="title" content="$title"/>
+</head>
+</html>
+EOF;
+
+	file_put_contents ($path."/index.html", $contents);
+}
+
+// create path
+function make_path ($date)
+{
+	global $wui;
+
+	$months = [
+		"01"	=> "Jan"
+	,	"02"	=> "Feb"
+	,	"03"	=> "Mar"
+	,	"04"	=> "Apr"
+	,	"05"	=> "May"
+	,	"06"	=> "Jun"
+	,	"07"	=> "Jul"
+	,	"08"	=> "Aug"
+	,	"09"	=> "Sep"
+	,	"10"	=> "Oct"
+	,	"11"	=> "Nov"
+	,	"12"	=> "Dec"
+	];
+
+	$d = explode (".", $date);
+
+	$p = $wui["journal_dir"];
+	// create /journal
+	create_node (APP_PATH . $p, $wui["journal_name"]);
+	// create /journal/year
+	$p .= "/". $d[0];
+	create_node (APP_PATH . $p, $d[0]);
+	// create /journal/year/month
+	$p .= "/". $d[1];
+	create_node (APP_PATH . $p, $months[$d[1]]);
+
+	return $p;
+}
+
 try {
-	$node_parent = $_POST["e_node_parent"];
-	$node_name = $_POST["e_node_name"];
-	$e_title = $_POST["e_title"];
-	$e_pub_date = $_POST["e_publish-date"];
-	$e_author = $_POST["e_author"];
-	$e_content = $_POST["e_content"];
+	$node_parent	= $_POST["e_node_parent"];
+	$node_name		= $_POST["e_node_name"];
+	$e_title		= $_POST["e_title"];
+	$e_pub_date		= $_POST["e_publish_date"];
+	$e_pub_time		= $_POST["e_publish_time"];
+	$e_author		= $_POST["e_author"];
+	$e_content		= $_POST["e_content"];
+
+	$path = make_path ($e_pub_date);
+
+	if (null === $node_parent || "" === $node_parent) {
+		$node_parent = $path;
+	}
 
 	// check node name, if empty use date + title
 	if (null === $node_name || "" === $node_name) {
@@ -41,7 +102,7 @@ try {
 <html>
 <head>
 <meta name="title" content="$e_title"/>
-<meta name="publish-date" content="$e_pub_date"/>
+<meta name="publish_date" content="$e_pub_date $e_pub_time"/>
 <meta name="author" content="$e_author"/>
 </head>
 <body>
