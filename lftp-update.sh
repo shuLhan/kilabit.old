40a9ec9d2b3100c149f40d4d72a47a28d5bba299
Mhd Sulhan <m.shulhan@gmail.com>
2014-07-29 13:58:54 +0700

    [kilabit] update lftp to allow sync specific directory.

--- lftp-update.sh
+++ lftp-update.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-lftp -e "mirror -X '.git/*' -X '_kemxtri/*' -X '_sima/*' -R --verbose=3 --delete; exit;" -p 22 -u kilatinf sftp://ftp.kilabit.info/home/kilatinf/public_html/
+dir=${1-""}
+
+echo $dir
+
+lftp -e "lcd $dir; mirror -X '.git/*' -X '_kemxtri/*' -X '_sima/*' -R --verbose=3 --delete; exit;" -p 22 -u kilatinf sftp://ftp.kilabit.info/home/kilatinf/public_html/$dir
