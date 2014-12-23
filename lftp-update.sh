e78794f7c287f1e316078c106ff2637cf4c0756f
Mhd Sulhan <m.shulhan@gmail.com>
2014-12-24 03:06:57 +0700

    lftp-update.sh: fix single file and directory upload.

--- lftp-update.sh
+++ lftp-update.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 node=${1-"."}
+user=kilatinf
+url=sftp://kilabit.info/home/kilatinf/public_html/
 
 if [ -d $node ]; then
 	echo "update directory $node"
-	lftp -e "lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -p 22 -u kilatinf sftp://ftp.kilabit.info/home/kilatinf/public_html/$node
+	lftp -e "mkdir -p $node; cd $node; lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -p 22 -u ${user} ${url}
 else
 	dir=`dirname $node`
 	echo "update file $node on $dir"
-	lftp -e "put $node; exit;" -p 22 -u kilatinf sftp://ftp.kilabit.info/home/kilatinf/public_html/$dir/
+	lftp -e "mkdir -p $dir; cd $dir; put $node; exit;" -p 22 -u ${user} ${url}
 fi
