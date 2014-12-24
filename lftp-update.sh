8c0503739cfc0762379f716bdaa210b1b25272d6
Mhd Sulhan <m.shulhan@gmail.com>
2014-12-25 04:41:13 +0700

    Suppress error message.

--- lftp-update.sh
+++ lftp-update.sh
@@ -6,9 +6,9 @@ url=sftp://kilabit.info/home/kilatinf/public_html/
 
 if [ -d $node ]; then
 	echo "update directory $node"
-	lftp -e "mkdir -p $node; cd $node; lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -p 22 -u ${user} ${url}
+	lftp -e "mkdir -pf $node; cd $node; lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -p 22 -u ${user} ${url}
 else
 	dir=`dirname $node`
 	echo "update file $node on $dir"
-	lftp -e "mkdir -p $dir; cd $dir; put $node; exit;" -p 22 -u ${user} ${url}
+	lftp -e "mkdir -pf $dir; cd $dir; put $node; exit;" -p 22 -u ${user} ${url}
 fi
