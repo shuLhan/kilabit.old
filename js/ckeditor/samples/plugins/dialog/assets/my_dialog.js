1031f3c56a2526edc1fa3a2e2104336b81ec9279
Mhd Sulhan <m.shulhan@gmail.com>
2014-12-29 04:00:57 +0700

    Update ckeditor.

--- js/ckeditor/samples/plugins/dialog/assets/my_dialog.js
+++ js/ckeditor/samples/plugins/dialog/assets/my_dialog.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.dialog.add( 'myDialog', function() {
+CKEDITOR.dialog.add( 'myDialog', function( editor ) {
 	return {
 		title: 'My Dialog',
 		minWidth: 400,
