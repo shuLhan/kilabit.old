56d75cc7e7c8dfffddeb14834f111594f11e47e4
Mhd Sulhan <ms@kilabit.info>
2016-01-09 17:01:27 +0700

    lftp-update: set debug option.

--- lftp-update.sh
+++ lftp-update.sh
@@ -6,9 +6,9 @@ url=sftp://kilabit.info/home/kilatinf/public_html/
 
 if [ -d $node ]; then
 	echo "update directory $node"
-	lftp -e "mkdir -pf $node; cd $node; lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -p 22 -u ${user} --env-password ${url}
+	lftp -e "mkdir -pf $node; cd $node; lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -p 22 -u ${user} --env-password -d ${url}
 else
 	dir=`dirname $node`
 	echo "update file $node on $dir"
-	lftp -e "mkdir -pf $dir; cd $dir; put $node; exit;" -p 22 -u ${user} --env-password ${url}
+	lftp -e "mkdir -pf $dir; cd $dir; put $node; exit;" -p 22 -u ${user} --env-password -d ${url}
 fi
