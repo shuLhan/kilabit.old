fdafdee82cf62745b1c01ab5422b17d253046966
Mhd Sulhan <ms@kilabit.info>
2016-01-11 04:57:20 +0700

    Use ssh config and public key to connect to host.

--- lftp-update.sh
+++ lftp-update.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 node=${1-"."}
-user=kilatinf
-url=sftp://kilabit.info/home/kilatinf/public_html/
+url=sftp://sftp.kilabit.info/home/kilatinf/public_html/
 
 if [ -d $node ]; then
 	echo "update directory $node"
-	lftp -e "mkdir -pf $node; cd $node; lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -p 22 -u ${user} --env-password -d ${url}
+	lftp -e "mkdir -pf $node; cd $node; lcd $node; mirror -R --verbose=3 -X '.git/*' --delete; exit;" -d ${url}
 else
 	dir=`dirname $node`
 	echo "update file $node on $dir"
-	lftp -e "mkdir -pf $dir; cd $dir; put $node; exit;" -p 22 -u ${user} --env-password -d ${url}
+	lftp -e "mkdir -pf $dir; cd $dir; put $node; exit;" -d ${url}
 fi
